4f92d8f357ac62c86564772e0636188aadad0be7
deimonn <deimonn@outlook.com>
2025-08-23 22:51:45 -0300

    Commit main template and update base styles

---
+++ src/main.js
@@ -0,0 +1,70 @@
+/*── src/main.js ── Main template code ──*
+ │
+ │ Copyright (c) 2024-2025 Deimonn
+ │
+ │ This file is licensed under the MIT License.
+ │
+ │ See https://deimonn.dev/license.txt for license information.
+ │
+ */
+
+// SPDX-License-Identifier: MIT
+
+// Sets and applies a theme site-wide.
+function setTheme(theme) {
+    const html = document.documentElement;
+    const themeIcon = document.getElementById("dei-themeicon");
+
+    // Save theme setting.
+    localStorage.setItem("theme", theme);
+
+    // Update document class and theme icon.
+    if (theme === "light") {
+        html.className = "";
+        themeIcon.setAttribute("name", "sun-fill");
+    } else if (theme === "dark") {
+        html.className = "sl-theme-dark";
+        themeIcon.setAttribute("name", "moon-fill");
+    } else {
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            html.className = "sl-theme-dark";
+        } else {
+            html.className = "";
+        }
+
+        themeIcon.setAttribute("name", "circle-half");
+    }
+}
+
+// Back-to-top button.
+(function() {
+    const backToTop = document.getElementById("dei-backtotop");
+
+    // On scroll, display/hide the button.
+    document.addEventListener("scroll", () => {
+        if (window.scrollY > 800) {
+            backToTop.setAttribute("style", "");
+        } else {
+            backToTop.setAttribute("style", "display: none");
+        }
+    });
+
+    // On click, scroll the window.
+    backToTop.addEventListener("click", () => {
+        window.scrollTo(0, 0);
+        window.location.hash = "";
+    });
+})();
+
+// Theme selection menu.
+(function() {
+    const themeMenu = document.getElementById("dei-thememenu");
+
+    // On theme selected, update theme.
+    themeMenu.addEventListener("sl-select", (event) => {
+        setTheme(event.detail.item.value);
+    });
+})();
+
+// Theme initialization.
+setTheme(localStorage.getItem("theme") ?? "system");
